941797a391f806889a37e5ee7f6a725e3eb42cf9
Diego Escalante <diego.escalante01@gmail.com>
2015-12-12 02:52:18 -0700

    Started camera work

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+  [SerializeField] private float smoothness = 3f;
+  private Transform target;
+
+  //For screen shakes.
+  private float duration = 1f;
+  //===================================================================================================================
+
+  private void Start(){
+    target = GameObject.FindWithTag("Monster").transform;
+  }
+
+  //===================================================================================================================
+
+  private void FixedUpdate(){
+    
+  }
+
+  private void follow(){
+    Vector3 newPos = new Vector3(target.position.x, transform.position.y, transform.position.z);
+
+    if(transform.position == newPos) return;
+    if(Vector2.Distance(transform.position, target.position) < 0.1f) transform.position = newPos;
+    else transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * smoothness);
+  }
+
+  private void shake(){
+
+  }
+
+  private IEnumerator shaking(){
+    float endTime = Time.time + duration;
+
+    while(Time.time < endTime){
+      //SHAKE IT.
+      yield return new WaitForFixedUpdate();
+    }
+  }
+}
---
+++ Assets/Scripts/MonsterBehavior.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+  public class MonsterBehavior : MonoBehaviour {
+
+  private float sizeMultiplier = 1f;
+  private float camSize;
+
+  //===================================================================================================================
+
+  private void Start() {
+    //Get the original camera size.
+    camSize = Camera.main.orthographicSize;
+  }
+
+  //===================================================================================================================
+
+  private void setSize(float f, bool relative=true) {
+    //Increase the size of the monster, keeping the camera perfectly scaled to it.
+    sizeMultiplier = relative ? sizeMultiplier + f : f;
+    transform.localScale = new Vector3(1 * sizeMultiplier, 1 * sizeMultiplier, 1);
+    Camera.main.orthographicSize = camSize * sizeMultiplier;
+  }
+}
