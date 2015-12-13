e5a6e16c8d2375a5a9cf130532db6480a282562a
Diego Escalante <diego.escalante01@gmail.com>
2015-12-13 02:05:56 -0700

    Added basic game controller

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+  private HealthManager monsterHM;
+  private HealthManager playerHM;
+
+  //===================================================================================================================
+
+  private void Start() {
+    monsterHM = GameObject.FindWithTag("Monster").GetComponent<HealthManager>();
+    playerHM = GameObject.FindWithTag("Player").GetComponent<HealthManager>();
+  }
+
+  //===================================================================================================================
+
+  private void Update() {
+    if(Input.GetKeyDown("r")) restart();
+    if(loseCheck()) Invoke("restart", 3); 
+  }
+
+  private bool loseCheck() {
+    return !monsterHM.enabled && !playerHM.enabled;
+  }
+
+  private void restart() {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+  }
+}
