37877a98dd41e9fa7fadda60269aecadcb7a12a9
Diego Escalante <diego.escalante01@gmail.com>
2015-12-14 15:16:35 -0700

    Rejoice, we are now doing both themes Controls are two buttons now.

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -20,13 +20,24 @@ public class InputManager : MonoBehaviour {
   public static event InputButton shieldPressed;
   public static event InputButton shieldReleased;
 
+  private bool alreadyDown = false;
+
   //===================================================================================================================
 
   private void Update() {
     if(Input.GetButtonDown("Jump") && jumpPressed != null) jumpPressed();
-    if(Input.GetButtonDown("Attack") && attackPressed != null) attackPressed();
     if(Input.GetButtonDown("Shield") && shieldPressed != null) shieldPressed();
     if(Input.GetButtonUp("Shield") && shieldReleased != null) shieldReleased();
+
+
+
+    // if(Input.GetButtonDown("Attack") && attackPressed != null) attackPressed();
     if(horizontalAxis != null) horizontalAxis(Input.GetAxisRaw("Horizontal"));
+
+    if(Input.GetButton("Left") && Input.GetButton("Right") && !alreadyDown) {
+      alreadyDown = true;
+      if(attackPressed != null) attackPressed();
+    }
+    else if(!(Input.GetButton("Left") && Input.GetButton("Right")) && alreadyDown) alreadyDown = false;
   }
 }
